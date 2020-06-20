30912f06e46c366579fdb5f9e03aa3b0556885f4
Daniel Shapero <shapero.daniel@gmail.com>
2020-06-20 16:45:15 -0700

    Test pair of adjacent triangles

--- test/test_topology.py
+++ test/test_topology.py
@@ -19,16 +19,13 @@ def test_edge():
     topology.set_num_cells(0, 2)
     topology.set_num_cells(1, 1)
 
+    # Check that there are no non-zero ∂∂-products
     topology.set_cell(1, 0, (0, 1), (-1, +1))
     products = topology.compute_nonzero_boundary_products()
     assert products == []
 
-
-def test_bad_edge():
-    topology = zmsh.Topology(1)
-    topology.set_num_cells(0, 2)
-    topology.set_num_cells(1, 1)
-
+    # Now make an edge with two endpoints and check that the vertex * edge
+    # matrix is non-zero
     topology.set_cell(1, 0, (0, 1), (+1, +1))
     products = topology.compute_nonzero_boundary_products()
     dimension, matrix = products[0]
@@ -48,23 +45,40 @@ def test_triangle():
 
     topology.set_cell(2, 0, (0, 1, 2), (+1, +1, +1))
 
+    # Check that there are no non-zero ∂∂-products
     products = topology.compute_nonzero_boundary_products()
     assert products == []
 
-
-def test_bad_triangle():
-    topology = zmsh.Topology(2)
-
-    topology.set_num_cells(0, 3)
-    topology.set_num_cells(1, 3)
-    topology.set_num_cells(2, 1)
-
-    topology.set_cell(1, 0, (0, 1), (-1, +1))
-    topology.set_cell(1, 1, (1, 2), (-1, +1))
-    topology.set_cell(1, 2, (2, 0), (-1, +1))
-
+    # Now change the triangle so that one of the edges is reversed and check
+    # that the triangle * edge matrix is non-zero
     topology.set_cell(2, 0, (0, 1, 2), (+1, -1, +1))
 
     products = topology.compute_nonzero_boundary_products()
     dimension, matrix = products[0]
     assert dimension == 1
+
+
+def test_triangle_pair():
+    topology = zmsh.Topology(2)
+    topology.set_num_cells(dimension=0, num_cells=4)
+    topology.set_num_cells(dimension=1, num_cells=5)
+    topology.set_num_cells(dimension=2, num_cells=2)
+
+    topology.set_cell(1, 0, (0, 1), (-1, +1))
+    topology.set_cell(1, 1, (1, 2), (-1, +1))
+    topology.set_cell(1, 2, (2, 0), (-1, +1))
+    topology.set_cell(1, 3, (0, 3), (-1, +1))
+    topology.set_cell(1, 4, (3, 1), (-1, +1))
+
+    topology.set_cell(2, 0, (0, 1, 2), (+1, +1, +1))
+    topology.set_cell(2, 1, (0, 3, 4), (-1, +1, +1))
+
+    # Check that there are no non-zero ∂∂-products
+    products = topology.compute_nonzero_boundary_products()
+    assert products == []
+
+    # Make a topologically valid transformation -- reverse all the incidences
+    # of a single cell
+    topology.set_cell(2, 1, (0, 3, 4), (+1, -1, -1))
+    products = topology.compute_nonzero_boundary_products()
+    assert products == []
