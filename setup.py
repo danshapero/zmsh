1106bb8cac6a180e7ef6c5e021edad40a41c516c
Daniel Shapero <shapero.daniel@gmail.com>
2020-06-20 18:24:23 -0700

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zmsh',
+    version='0.0.1',
+    license='proprietary',
+    description='mesh topology transformation via SMT',
+    author='Daniel Shapero',
+    packages=find_packages(exclude=['doc', 'test']),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'z3-solver'
+    ]
+)
