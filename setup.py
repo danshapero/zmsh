e2ebcacab440f0eb26f0e6dbbf8bb1255c2d077e
Daniel Shapero <shapero.daniel@gmail.com>
2021-11-01 16:32:41 -0700

    add MPLv2 license

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="zmsh",
     version="0.0.1",
-    license="proprietary",
+    license="MPL 2.0",
     description="mesh topology transformation via SMT",
     author="Daniel Shapero",
+    author_email="shapero@uw.edu",
     packages=find_packages(exclude=["doc", "test"]),
     install_requires=["numpy", "scipy", "z3-solver"],
 )
