82aa57fdfd71479d1c854d217e5748662cea5dc5
Daniel Shapero <shapero.daniel@gmail.com>
2022-09-09 15:30:14 -0700

    Add circumcircle function

--- zmsh/predicates.py
+++ zmsh/predicates.py
@@ -7,6 +7,17 @@ def _homogeneous(x):
     return (1,) + tuple(Fraction(x_i) for x_i in x)
 
 
+def _parabolic_lift(x):
+    fx = tuple(Fraction(x_i) for x_i in x)
+    square_norm = sum(x_i**2 for x_i in fx)
+    return (1,) + fx + (square_norm,)
+
+
 def volume(*args):
     A = sympy.Matrix([_homogeneous(x) for x in args])
+    return np.float64(sympy.det(A))
+
+
+def circumcircle(*args):
+    A = sympy.Matrix([_parabolic_lift(x) for x in args])
     return np.float64(sympy.det(A))
