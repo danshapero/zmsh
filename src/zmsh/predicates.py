d0fe8b0e34156fd878a17e2612c55c18302694dc
Daniel Shapero <shapero.daniel@gmail.com>
2022-12-24 18:04:02 -0800

    Cut sympy, hand-code the determinant

--- src/zmsh/predicates.py
+++ src/zmsh/predicates.py
@@ -1,6 +1,20 @@
 import numpy as np
 from fractions import Fraction
-import sympy
+
+
+def _determinant(A):
+    if A.shape == (2, 2):
+        return A[1, 1] * A[0, 0] - A[1, 0] * A[0, 1]
+
+    if A.shape[0] != A.shape[1]:
+        raise ValueError("Matrix must be square!")
+
+    subdets = [
+        A[0, k] * _determinant(np.delete(np.delete(A, 0, 0), k, 1))
+        for k in range(A.shape[0])
+    ]
+
+    return sum(subdets[::2]) - sum(subdets[1::2])
 
 
 def _homogeneous(x):
@@ -14,10 +28,10 @@ def _parabolic_lift(x):
 
 
 def volume(*args):
-    A = sympy.Matrix([_homogeneous(x) for x in args])
-    return np.float64(sympy.det(A))
+    A = np.array([_homogeneous(x) for x in args], dtype=object)
+    return np.float64(_determinant(A))
 
 
 def circumcircle(*args):
-    A = sympy.Matrix([_parabolic_lift(x) for x in args])
-    return np.float64(sympy.det(A))
+    A = np.array([_parabolic_lift(x) for x in args], dtype=object)
+    return np.float64(_determinant(A))
