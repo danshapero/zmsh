872b1cc96d396086223fae2b860fffe7758f3aba
Daniel Shapero <shapero.daniel@gmail.com>
2022-12-31 17:25:50 -0800

    Placate flake8

--- src/zmsh/__init__.py
+++ src/zmsh/__init__.py
@@ -3,3 +3,16 @@ from .topology import Topology
 from .geometry import compositions, Geometry
 from .convex_hull import ConvexHullMachine, convex_hull
 from .plotting import visualize
+
+__all__ = [
+    "Geometry",
+    "Topology",
+    "ConvexHullMachine",
+    "convex_hull",
+    "visualize",
+    "transformations",
+    "simplicial",
+    "predicates",
+    "compositions",
+    "examples",
+]
